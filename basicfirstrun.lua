77de9be8847b6d24b75a6fe1f1c1b4df90970964
Rohan Iyer <rohan.iyer1999@gmail.com>
2014-11-19 20:56:44 -0800

    debugs

--- basicfirstrun.lua
+++ basicfirstrun.lua
@@ -2,27 +2,37 @@ scriptId = 'com.buildncode.codeday.basicfirstrun'
 
 recGesture = false
 inTabMenu = false
-
 function onPoseEdge(pose, edge)
+	myo.debug("went in")
+	myo.debug(pose)
 	if pose == "pinkyToThumb" and edge == "on" then
-		if !recGesture then
+		myo.debug("pinkyToThumb1")
+		if recGesture==false then
 			recGesture = true
 		elseif recGesture then
 			recGesture = false
 		end
 	end
-	elseif pose == "fist" and edge == "on" then
-    	-- i dunno bruh
-    elseif pose == "waveIn" and edge == "on" then
-    	leftTab()
-    elseif pose == "waveOut" and edge == "on" then
-    	rightTab()
+	if recGesture then
+		if pose == "fist" and edge == "on" then
+	    	-- i dunno bruh
+	    	myo.debug("fist1")
+	    elseif pose == "fingersSpread" and edge == "on" then
+	    	myo.debug("fingersSpread1")
+	    	commandTab()
+	    elseif pose == "waveIn" and edge == "on" then
+	    	myo.debug("waveIn1")
+	    	leftTab()
+	    elseif pose == "waveOut" and edge == "on" then
+	    	myo.debug("waveOut1")
+	    	rightTab()
+	    end
     end
 end
 
 function onPeriodic()
-	myo.controlMouse(true)
-	myo.debug(myo.mouseControlEnabled())
+	--myo.controlMouse(true)
+	--myo.debug(myo.mouseControlEnabled())
 end	
 
 function onForegroundWindowChange(app, title)
@@ -32,10 +42,10 @@ function activeAppName()
     return "Output Everything"
 end
 function onActiveChange(isActive)
-    myo.debug("onActiveChange")
+    --myo.debug("onActiveChange")
 end
 function commandTab()
-	if !inTabMenu then
+	if inTabMenu==false then
 		myo.keyboard("left_command", "down")
 		myo.keyboard("tab", "press")
 		inTabMenu = true
