c38e293d1e728fd5274901e0456ca6dc8a22f50a
Rohan Iyer <rohan.iyer1999@gmail.com>
2014-11-19 20:28:24 -0800

    added command tabbing

--- basicfirstrun.lua
+++ basicfirstrun.lua
@@ -1,11 +1,22 @@
 scriptId = 'com.buildncode.codeday.basicfirstrun'
 
 recGesture = false
+inTabMenu = false
 
 function onPoseEdge(pose, edge)
-	if(pose == "pinkyToThumb")
-	if pose == "fist" and edge == "on" then
-    	myo.keyboard()
+	if pose == "pinkyToThumb" and edge == "on" then
+		if !recGesture then
+			recGesture = true
+		elseif recGesture then
+			recGesture = false
+		end
+	end
+	elseif pose == "fist" and edge == "on" then
+    	-- i dunno bruh
+    elseif pose == "waveIn" and edge == "on" then
+    	leftTab()
+    elseif pose == "waveOut" and edge == "on" then
+    	rightTab()
     end
 end
 
@@ -24,11 +35,23 @@ function onActiveChange(isActive)
     myo.debug("onActiveChange")
 end
 function commandTab()
-
+	if !inTabMenu then
+		myo.keyboard("left_command", "down")
+		myo.keyboard("tab", "press")
+		inTabMenu = true
+	elseif inTabMenu then
+		myo.keyboard("left_command", "up")
+		myo.keyboard("tab", "down")
+		inTabMenu = false
+	end
 end
 function rightTab()
-
+	if inTabMenu then
+		myo.keyboard("right_arrow")
+	end
 end
 function leftTab()
-	
+	if inTabMenu then
+		myo.keyboard("right_arrow")
+	end
 end
